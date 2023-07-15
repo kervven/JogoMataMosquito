2f21d1a1c80bf344f2d65bda6866dbc566a1295d
Kerven Kildhery <kervensilvva@gmail.com>
2023-07-15 19:08:42 -0300

    gerando posicao randomica

--- jogo.js
+++ jogo.js
@@ -2,10 +2,33 @@ var altura = 0
 var largura = 0
 
 
+
 function ajustaTamanhoPalcoJogo() {
-    var altura = window.innerHeight
-    var largura = window.innerWidth
+    altura = window.innerHeight
+    largura = window.innerWidth
     console.log(altura, largura)
 }
 
-ajustaTamanhoPalcoJogo() 
+ajustaTamanhoPalcoJogo()
+
+//os codigos dessa função precisaram ser encapsulados em uma função porque, como é necessário rodar esse codigo depois da existencia do body la no html, essa função precisa ser chamada dentro do body, que já é o momento em que ela é criada.
+//Caso o contratrio, a imagem do mosquito nao sera criada, pois a referencia do js no head vem primeiro do que a criação do body html
+function posicaoRandomica() {
+    var posicaoX = Math.floor(Math.random() * largura) - 90
+    var posicaoY = Math.floor(Math.random() * altura) - 90
+
+    posicaoX = posicaoX < 0 ? 0 : posicaoX
+    posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+    console.log(posicaoX, posicaoY)
+
+    //criar o elemento html
+    var mosquito = document.createElement('img')
+    mosquito.src = 'imagens/mosca.png'
+    mosquito.className = 'mosquito1'
+    mosquito.style.left = posicaoX + 'px'
+    mosquito.style.top = posicaoY + 'px'
+    mosquito.style.position = 'absolute'
+
+    document.body.appendChild(mosquito)
+}
