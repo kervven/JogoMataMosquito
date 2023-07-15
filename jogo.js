36d400b07a9cfd9a072f84f41f7696154ccc9cdd
Kerven Kildhery <kervensilvva@gmail.com>
2023-07-15 19:54:35 -0300

    fazendo os mosquitos ficar de lados diferentes quando forem gerados

--- jogo.js
+++ jogo.js
@@ -25,7 +25,7 @@ function posicaoRandomica() {
     //criar o elemento html
     var mosquito = document.createElement('img')
     mosquito.src = 'imagens/mosca.png'
-    mosquito.className = tamanhoAleatorio()
+    mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
     mosquito.style.left = posicaoX + 'px'
     mosquito.style.top = posicaoY + 'px'
     mosquito.style.position = 'absolute'
@@ -43,5 +43,16 @@ function tamanhoAleatorio(){
             return 'mosquito2'
         case 2:
             return 'mosquito3'
+    }
+}
+
+function ladoAleatorio(){
+    var classe = Math.floor(Math.random() * 2)
+
+    switch(classe) {
+        case 0:
+            return 'ladoA'
+        case 1:
+            return 'ladoB'
     }
 }
