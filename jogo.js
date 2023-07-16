4e05acdce7db31819de6b5f7846156b958cfde3b
Kerven Kildhery <kervensilvva@gmail.com>
2023-07-15 22:58:28 -0300

    controlando os pontos de vida

--- jogo.js
+++ jogo.js
@@ -1,6 +1,6 @@
 var altura = 0
 var largura = 0
-
+var vidas = 1
 
 
 function ajustaTamanhoPalcoJogo() {
@@ -18,6 +18,13 @@ function posicaoRandomica() {
     //aqui vamos remover o mosquito anterior caso exista
     if(document.getElementById('mosquito')) {
         document.getElementById('mosquito').remove()
+
+        if(vidas > 3) {
+            alert('game over')
+        } else {
+        document.getElementById('v' + vidas).src = 'imagens/coracao_vazio.png'
+        vidas++
+        }
     }
 
     var posicaoX = Math.floor(Math.random() * largura) - 90
@@ -36,6 +43,9 @@ function posicaoRandomica() {
     mosquito.style.top = posicaoY + 'px'
     mosquito.style.position = 'absolute'
     mosquito.id = 'mosquito'
+    mosquito.onclick = function(){
+        this.remove()
+    }
 
     document.body.appendChild(mosquito)
 }
